e2c7b97c14931f44c8789d9cb98e8c643f543c27
ukusamukad <gangneux.alex@gmail.com>
2021-12-12 18:55:34 +0100

    finalisation de la classe Admin (javadoc et méthodes)

--- javas3/src/JavaObject/Admin.java
+++ javas3/src/JavaObject/Admin.java
@@ -1,5 +1,16 @@
 package JavaObject;
 
+import java.util.ArrayList;
+
+/**
+ * La classe Admin , permet de crée un Administrateur.
+ * Il pourra par la suite utiliser toute les méthodes disponible dans cette classe.
+ * Administrateur aura tout les droits de suppression, d'ajout et de modification sur les autres utilisateurs (Entreprise et Client).
+ * @see DAO
+ * @author gangn
+ *
+ */
+
 public class Admin {
 
 	private String nom;
@@ -7,7 +18,7 @@ public class Admin {
 	private String prenom;
 
 	private String password;
-	private DAO d = new DAO();
+	private DAO instance = new DAO();
 
 	/**
 	 * Ce constructeur permet de créer un administrateur , chaque administrateur est
@@ -17,6 +28,7 @@ public class Admin {
 	 * @param nom    -String
 	 * @param prenom -String
 	 * @param email  -String
+	 * @author Gangneux Alexis
 	 */
 	public Admin(String nom, String prenom, String password, String email) {
 		this.nom = nom;
@@ -28,6 +40,7 @@ public class Admin {
 	 * Ce getter permet de récupérer le nom d'un administrateur
 	 * 
 	 * @return Un nom de type String
+	 * @author Gangneux Alexis
 	 */
 	private String getNom() {
 		return this.nom;
@@ -37,6 +50,7 @@ public class Admin {
 	 * Ce setter permet de modifier le nom de l'administrateur
 	 * 
 	 * @param nom -String
+	 * @author Gangneux Alexis
 	 */
 	private void setNom(String nom) {
 		this.nom = nom;
@@ -46,6 +60,7 @@ public class Admin {
 	 * Ce getter permet de récupérer le prénom d'un administrateur
 	 * 
 	 * @return Un prénom de type String
+	 * @author Gangneux Alexis
 	 */
 	private String getPrenom() {
 		return this.prenom;
@@ -55,6 +70,7 @@ public class Admin {
 	 * Ce setter permet de modifier le Prenom d'un administrateur
 	 * 
 	 * @param prenom -String
+	 * @author Gangneux Alexis
 	 */
 	private void setPrenom(String prenom) {
 		this.prenom = prenom;
@@ -64,6 +80,7 @@ public class Admin {
 	 * Ce getter permet de récupérer le mot de passe d'un administrateur
 	 * 
 	 * @return Un mot de passe de type String
+	 * @author Gangneux Alexis
 	 */
 	private String getPassword() {
 		return this.password;
@@ -73,6 +90,7 @@ public class Admin {
 	 * Ce setter permet de modifier le mot de passe d'un administrateur
 	 * 
 	 * @param password -String
+	 * @author Gangneux Alexis
 	 */
 	private void setPassword(String password) {
 		this.password = password;
@@ -99,10 +117,11 @@ public class Admin {
 	 * @param ListClient[]   -Client
 	 * @param password       -String
 	 * @param vehicule       -Vehicule
+	 * @see ajouterEntreprise() -DAO
+	 * @author Gangneux Alexis
 	 */
 	public void ajouterEntreprise(String siret, String prenom, String nom, Integer numeroDeRue, String rue,
 			Integer codePostal, String ville, String pays, String numTelephone, String motsdepasses, String mailAdmin) {
-		DAO instance = new DAO();
 		instance.ajouterEntreprise(siret, prenom, nom, numeroDeRue, rue, codePostal, ville, pays, numTelephone,
 				motsdepasses, mailAdmin);
 	}
@@ -114,10 +133,11 @@ public class Admin {
 	 * 
 	 * @param Entreprise à supprimer -Entreprise
 	 * @return
-	 * @see supprimerEntreprise()
+	 * @see supprimerEntreprise() -DAO
+	 * @author Gangneux Alexis
 	 */
 	public void supprimerEntreprise(Entreprise entreprise) {
-		DAO instance = new DAO();
+		
 		instance.supprimerEntreprise(entreprise);
 	}
 
@@ -135,12 +155,12 @@ public class Admin {
 	 * @param numTelephone -String
 	 * @param numTelephone -Long
 	 * @param entreprise   -Entreprise
-	 * @author gangn
-	 * @return
+	 * @author Gangneux Alexis
+	 * @see ajouterclient() -DAO
 	 */
 	public void ajouterClient(String prenom, String nom, Integer numeroDeRue, String rue, Integer codePostal,
 			String ville, String pays, String numTelephone, String mailAdmin) {
-		DAO instance = new DAO();
+		
 		instance.ajouterclient(prenom, nom, numeroDeRue, rue, codePostal, ville, pays, numTelephone, mailAdmin);
 
 	}
@@ -151,10 +171,11 @@ public class Admin {
 	 * fonctionne.
 	 * 
 	 * @param Client à supprimer -Client
-	 * @return
+	 * @see delclient() -DAO
+	 * @author Gangneux Alexis
 	 */
 	public void supprimerClient(Client client) {
-		DAO instance = new DAO();
+		
 		instance.delclient(client);
 		// blablatest
 	}
@@ -163,10 +184,11 @@ public class Admin {
 	 * Cette méthode permet de modifier les parametres d'un client.
 	 * 
 	 * @param Client a modifier -Client
-	 * @return
+	 * @see modifclient() -DAO
+	 * @author Gangneux Alexis
 	 */
 	public void ModifClient(Client client) {
-		DAO instance = new DAO();
+		
 		instance.modifclient(client);
 	}
 
@@ -174,10 +196,11 @@ public class Admin {
 	 * Cette méthode permet de modifier les parametres d'une entreprise.
 	 * 
 	 * @param Entreprise à modifier -Entreprise
-	 * @return
+	 * @see modifEntreprise() -DAO
+	 * @author Gangneux Alexis
 	 */
 	public void ModifEntreprise(Entreprise entreprise) {
-		DAO instance = new DAO();
+		
 		instance.modifEntreprise(entreprise);
 
 	}
@@ -189,10 +212,17 @@ public class Admin {
 	 * 
 	 * @param adresseMail (Concaténation du Nom.Prénom@...fr) -String
 	 * @param password    -String
-	 * @return
+	 * @see listAdmin() -DAO
+	 * @author Gangneux Alexis
 	 */
-	public void connexion() {
-		// TODO
+	public void connexion(Admin admin) {
+		ArrayList<Admin> listAdmin = instance.listAdmin();
+		for(Admin a : listAdmin) {
+			if(admin == a) {
+				System.out.println("Connexion réussie");
+			}
+		}
+		System.out.println("Connexion échouer, enregistrez-vous dans la base de donnée");
 	}
 
 }
