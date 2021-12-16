8dc450eb42ea6c66f4b61baedfd443529ee56228
user <user@DESKTOP-HGLMJSU>
2021-12-16 20:18:54 +0100

    suppression ancienne erreur

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"JavaObject","l":"Admin"},{"l":"All Classes","u":"allclasses-index.html"},{"p":"JavaObject","l":"Client"},{"p":"JavaObject","l":"Commande"},{"p":"JavaObject","l":"DAO"},{"p":"JavaObject","l":"Entreprise"},{"p":"JavaObject","l":"Singleton"},{"p":"JavaObject","l":"Tournee"},{"p":"JavaObject","l":"Vehicule"}];updateSearchResults();
